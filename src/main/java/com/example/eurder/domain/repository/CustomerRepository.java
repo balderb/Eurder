17a506604832c35ecbe31de8391d17f0608f7eda
Balder Bongaerts <bbongaerts@gmail.com>
2023-12-06 14:55:51 +0100

    feat: refactor arrayList to hashMap and add additional methods on repository

--- src/main/java/com/example/eurder/domain/repository/CustomerRepository.java
+++ src/main/java/com/example/eurder/domain/repository/CustomerRepository.java
@@ -3,20 +3,27 @@ package com.example.eurder.domain.repository;
 import com.example.eurder.domain.Customer;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Repository
 public class CustomerRepository {
    
-   private final List<Customer> customers;
+   private final Map<String, Customer> customers;
    
    public CustomerRepository() {
-      customers = new ArrayList<>();
-      customers.add(new Customer("John", "Doe", "john.Doe@gmail.com", "DreamSquare 5, 1711 Dreamville", "052113355"));
+      this.customers = new HashMap<>();
+      Customer seadCustomer = new Customer("John", "Doe", "john.Doe@gmail.com", "DreamSquare 5, 1711 Dreamville", "052113355");
    }
    
    public void addNewCustomer(Customer customer) {
-      customers.add(customer);
+      customers.put(customer.getCustomerId(), customer);
+   }
+   
+   public Customer getCustomerById(String customerId) {
+      return customers.get(customerId);
+   }
+   
+   public Collection<Customer> getAllCustomers() {
+      return customers.values();
    }
 }
